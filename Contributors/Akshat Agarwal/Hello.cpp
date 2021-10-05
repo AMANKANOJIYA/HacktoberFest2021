a9c2bfea993f20e320d95bf9390c8218c2564f00
Akshat Agarwal <88102754+akshatx@users.noreply.github.com>
2021-10-05 17:29:35 +0530

    Hello in CPP

---
+++ Contributors/Akshat Agarwal/Hello.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define cp() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+int main(){
+    cp();
+    cout<<"Hello World!"<<endl;
+    cout<<"I am Akshat Agarwal."<<endl;
+    cout<<"I am from India."<<endl;
+    cout<<"I am from IIT ISM Dhanbad and in 2nd year."<<endl;
+    cout<<"Email ID : akshat271102@gmail.com"<<endl;
+    cout<<"Github : akshatx"<<endl;
+    return 0;
+}

c75743918dc191c532e64895e589f6ca9255a16d
Akshat Agarwal <88102754+akshatx@users.noreply.github.com>
2021-10-13 21:42:16 +0530

    Adding Akshat.py
    Hey!
    Here is my Hello World code in python!

---
+++ Akshat.py
@@ -0,0 +1,7 @@
+print("Hello World !")
+print("I am Akshat Agarwal")
+print("I am From  Ghaziabad, Delhi NCR")
+print("I am IIT ISM Dhanbad and in 2nd year of Btech of Environmental Engineering.")
+print("Email ID : akshat@271102@gmail.com")
+print("Github : akshatx")
+

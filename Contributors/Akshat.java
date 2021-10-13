88da1d361e907dfd1e6149b1f8c52c5b57169dc8
Akshat Agarwal <88102754+akshatx@users.noreply.github.com>
2021-10-13 23:21:15 +0530

    Hello World in Java
    Hey!
    This is my Hello World Code in Java!

---
+++ Contributors/Akshat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args){
+        System.out.println("Hello, World !");
+        System.out.println("I am Akshat Agarwal");
+        System.out.println("I am From  Ghaziabad, Delhi NCR");
+        System.out.println("I am IIT ISM Dhanbad and in 2nd year of Btech of Environmental Engineering.");
+        System.out.println("Email ID : akshat@271102@gmail.com");
+        System.out.println("Github : akshatx");
+    }
+}

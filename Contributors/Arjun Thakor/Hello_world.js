23f968ef95907ed6e3fd0a03e21f060c147d1f08
The-Arjun-Thakor <84375918+The-Arjun-Thakor@users.noreply.github.com>
2021-10-14 11:54:13 +0530

    JavaScript program for hello world

---
+++ Contributors/Arjun Thakor/Hello_world.js
@@ -0,0 +1,14 @@
+// the hello world program
+document.write('Hello, World!');
+
+// the hello world program
+alert("Hello, World!");
+
+// the hello world program
+console.log('Hello World');
+
+document.write('I am Arjun Thakor');
+document.write('I am From Gujarat ');
+document.write('I am in sem 3 of ICT GANPAT UNIVERSITY');
+document.write('Email ID : arjunsinhthakor005@gmail.com');
+document.write('Github : The-Arjun-Thakor');

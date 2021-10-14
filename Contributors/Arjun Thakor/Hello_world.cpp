e3c0b7fa0a2048a669bacaefa8cb701209799786
The-Arjun-Thakor <84375918+The-Arjun-Thakor@users.noreply.github.com>
2021-10-14 11:45:29 +0530

    Hello world in c++.

---
+++ Contributors/Arjun Thakor/Hello_world.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World";
+    cout<<"I am Arjun Thakor";
+    cout<<"I am From Gujarat ";
+    cout<<"I am in sem 3 of ICT GANPAT UNIVERSITY";
+    cout<<"Email ID : arjunsinhthakor005@gmail.com";
+    cout<<"Github : The-Arjun-Thakor";
+    return 0;
+}

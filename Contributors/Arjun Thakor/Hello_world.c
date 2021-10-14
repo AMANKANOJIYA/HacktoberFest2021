ef99b274958f50f6ffae0cfdb6b6e430ff6e4000
The-Arjun-Thakor <84375918+The-Arjun-Thakor@users.noreply.github.com>
2021-10-14 12:01:36 +0530

    hello world program in c

---
+++ Contributors/Arjun Thakor/Hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   printf("Hello, World!");
+   printf("I am Arjun Thakor");
+   printf("I am From Gujarat ");
+   printf("I am in sem 3 of ICT GANPAT UNIVERSITY");
+   printf("Email ID : arjunsinhthakor005@gmail.com");
+   printf("Github : The-Arjun-Thakor");
+   return 0;
+}

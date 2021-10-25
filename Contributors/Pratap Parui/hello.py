f2094b9e229998dd6346b8629c40b11acb4cda7a
Pratap parui <52885495+pratap360@users.noreply.github.com>
2021-10-25 21:55:46 +0530

    Created hello.py

---
+++ Contributors/Pratap Parui/hello.py
@@ -0,0 +1,7 @@
+print("Hello World !")
+print("I am Pratap Parui")
+print("I am From Mumbai, India")
+print("I'm from NMite Mumbai and in 3rd year")
+print("Email ID : Pratapparui2001@gmail.com")
+print("Github : pratap360")
+

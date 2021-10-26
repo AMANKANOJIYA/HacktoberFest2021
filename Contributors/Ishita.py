607550813015bcd047a939827a74137373d950c9
Ishh1505 <93137470+Ishh1505@users.noreply.github.com>
2021-10-26 19:37:12 +0530

    Created Ishita.py
    A hello world program in python.

---
+++ Contributors/Ishita.py
@@ -0,0 +1,6 @@
+print("Hello World !")
+print("I am Ishita")
+print("I am From Varanasi, India")
+print("I am a 2nd Year Student at MNNIT.")
+print("Email : 123ishitavandanajitendra@gmail.com")
+print("Github : https://github.com/Ishh1505")
